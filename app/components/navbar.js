1aa7cb6c99b3ae98c9a44607e0193e6ccb8e8eb1
Muhammad Iqbal <muhammadiqbalid83@gmail.com>
2024-05-07 16:10:10 +0700

    first commit

---
+++ app/components/navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <div className="ring-1">
+      <div>
+        <p>Tetflix</p>
+      </div>
+      <div></div>
+    </div>
+  );
+}
